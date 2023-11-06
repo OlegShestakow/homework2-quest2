e71241ecacdc4f36789ca5eba52ca45f3e8fecd1
Oleg Shestakov <eithufter@gmail.com>
2023-11-06 18:12:08 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100; //Rub
-        int refill = 1300; //Rub
+        int refill = 1800; //Rub
         int percent;
         if (refill > 1000) {
             percent = 1;
         } else {
             percent = 0;
         }
-        int bonus = refill * percent / 100 + balance + refill;
-        System.out.println("Баланс счета: " + bonus);
+        int account = balance + refill;
+        System.out.println("Баланс счета: " + account);
+        int bonus = refill * 1 / 100;
+        System.out.println("Начислено бонусных рублей: " + bonus);
     }
 
 }
